1b5818b165248cdc988f81eb32580cf4b07dcf28
dokugo90 <dokugo90@gmail.com>
2023-05-06 17:47:51 +0000

    Ball Roller Game

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1738569.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 132389571, 132390079, 132390172, 132391225, 132392125, 132392192, 132392195, 132392196, 132392197, 132392205, 132392570, 132393199 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/132389583/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/132389584/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/132389582/1/ammo.js', 'preload' : true},
+];
